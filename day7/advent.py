7212f3b228a3dce85e6e9c39e5f762d0ea2a391f
Joseph Stanko <joseph.stanko@oracle.com>
2024-12-07 14:12:52 -0800

    format black

--- day7/advent.py
+++ day7/advent.py
@@ -4,15 +4,20 @@ def is_calibrated(test, first, rest):
     elif first > test:
         return False
     else:
-        return is_calibrated(test, first+rest[0], rest[1:]) or is_calibrated(test, first*rest[0], rest[1:]) or is_calibrated(test, int("{}{}".format(first,rest[0])), rest[1:])
+        return (
+            is_calibrated(test, first + rest[0], rest[1:])
+            or is_calibrated(test, first * rest[0], rest[1:])
+            or is_calibrated(test, int("{}{}".format(first, rest[0])), rest[1:])
+        )
+
 
 if __name__ == "__main__":
 
     sum = 0
     for line in open(0).read().splitlines():
-        equation = line.split(':')
+        equation = line.split(":")
         test = int(equation[0])
-        rest = list(map(int,equation[1].split()))
+        rest = list(map(int, equation[1].split()))
         if is_calibrated(test, rest[0], rest[1:]):
             sum += test
 
