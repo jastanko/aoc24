8fbfee1ea4cb377aed432b5fc0f26758cd15117d
Joseph Stanko <joseph.stanko@oracle.com>
2024-12-22 00:42:48 -0800

    day 22 part 2

--- day22/advent.py
+++ day22/advent.py
@@ -1,10 +1,26 @@
+from collections import Counter
+
 PRUNE = 16777216
+CYCLES = 2000
+BANANA_DICT = Counter()
 
 def pseudorandom(x):
-    for i in range(2000):
+    prices = [x % 10]
+    for i in range(CYCLES):
         x = (x * 64 ^ x) % PRUNE
         x = (x // 32 ^ x) % PRUNE
         x = (x * 2048 ^ x) % PRUNE
+        prices.append(x % 10)
+
+    deltas = [y - x for x, y in zip(prices[:-1], prices[1:])]
+    seq_dict = dict()
+    for i in range(4, len(prices)):
+        seq = str(deltas[i-4:i])
+        if not seq in seq_dict:
+            seq_dict[seq] = prices[i]
+
+    for k, v in seq_dict.items():
+        BANANA_DICT[k] += v
     return x
 
 if __name__ == "__main__":
@@ -13,3 +29,8 @@ if __name__ == "__main__":
         puzzle_input = data.read().splitlines()
 
     print(sum(map(pseudorandom, map(int, puzzle_input))))
+
+    bananas = max(BANANA_DICT, key=BANANA_DICT.get)
+    print(bananas)
+    print(BANANA_DICT[bananas])
+
