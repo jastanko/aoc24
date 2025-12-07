b9f3924a09f7efb86c4afc132046570e37ecde21
Joseph Stanko <joseph.stanko@oracle.com>
2025-12-06 22:10:48 -0800

    AOC 25 day 7.2

--- aoc25/day7.py
+++ aoc25/day7.py
@@ -1,20 +1,23 @@
+from collections import Counter
+
 if __name__ == "__main__":
 
     with open(0) as data:
         start, *puzzle_input = data.read().splitlines()
 
-    beams = set()
-    beams.add(start.find("S"))
+    beams = Counter()
+    beams[start.find("S")] = 1
     splits = 0
 
     for lines in puzzle_input:
         beams2 = beams.copy()
-        for beam in beams:
+        for beam, count in beams.items():
             if lines[beam] == "^":
                 splits += 1
-                beams2.remove(beam)
-                beams2.add(beam - 1)
-                beams2.add(beam + 1)
+                del beams2[beam]
+                beams2[beam - 1] += count
+                beams2[beam + 1] += count
         beams = beams2
 
     print(splits)
+    print(beams.total())
