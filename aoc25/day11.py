51ab98fd6a6903a652afb6e732276fc7eed7edc2
Joseph Stanko <joseph.stanko@oracle.com>
2025-12-12 23:41:30 -0800

    AOC 25 day 11.2

--- aoc25/day11.py
+++ aoc25/day11.py
@@ -1,9 +1,17 @@
-def num_paths(name, devices):
-    if devices[name][0] == "out":
+from collections import Counter
+
+
+def num_paths(name, goal, devices, cache):
+    if name == goal:
         return 1
+    if name == "out":
+        return 0
+    if name in cache:
+        return cache[name]
     sum = 0
     for out in devices[name]:
-        sum += num_paths(out, devices)
+        sum += num_paths(out, goal, devices, cache)
+    cache[name] = sum
     return sum
 
 
@@ -17,4 +25,14 @@ if __name__ == "__main__":
         name, list = line.split(":")
         devices[name] = list.split()
 
-    print(num_paths("you", devices))
+    paths = (
+        num_paths("svr", "fft", devices, Counter())
+        * num_paths("fft", "dac", devices, Counter())
+        * num_paths("dac", "out", devices, Counter())
+    )
+    paths += (
+        num_paths("svr", "dac", devices, Counter())
+        * num_paths("fft", "out", devices, Counter())
+        * num_paths("dac", "fft", devices, Counter())
+    )
+    print(paths)
